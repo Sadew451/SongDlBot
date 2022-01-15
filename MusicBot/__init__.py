fa18f4ab5b89003b75efde897b9b4dee5efd1dd1
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2022-01-15 20:55:16 +0530

    Update __init__.py

--- MusicBot/__init__.py
+++ MusicBot/__init__.py
@@ -8,6 +8,8 @@ API_ID =int(os.environ.get('API_ID'))
 API_HASH = (os.environ.get("API_HASH"))
 BOT_TOKEN = (os.environ.get("BOT_TOKEN"))
 
+LOGGER = logging.getLogger(__name__)
+
 SDBotz = Client("Music Bot", bot_token = BOT_TOKEN, api_id = API_ID, api_hash = API_HASH)
 
 SDBotz.run()
