46d9cb5277887bdb73e8699cf8ce40e7a5f95e8d
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2022-01-16 19:18:49 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,4 +1,9 @@
+from distutils.command.clean import clean
 import os
+import asyncio
+import wget
+from youtube_dl import YoutubeDL
+from youtubesearchpython import Searchhvideos
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters
 
@@ -11,12 +16,12 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 SDBotz = Client("Sample Bot", bot_token = BOT_TOKEN, api_id = API_ID, api_hash = API_HASH)
 
 START_MSG = """
-Hi i am a sample bot this bot is making on 
+Hi **{}** i am a sample bot this bot is making on 
 
 **Server** : [Heroku](Heroku.com)
 **Library** : [Pyrogram](https://github.com/pyrogram/pyrogram) 
 
-Bot By @SDBotsz."""
+__Bot By @SDBotsz.__"""
 
 REPLY_MARKUP = InlineKeyboardMarkup(
     [[
@@ -33,9 +38,68 @@ async def start(client, message):
                              reply_markup=REPLY_MARKUP,
                              disable_web_page_preview=True)
 
-@SDBotz.on_message(filters.private & filters.text | filters.media)
-async def AWBotz(client, message):
-    await message.copy(message.chat.id)
+@SDBotz.on_message(filters.text & filters.private & ~filters.command("start"))
+async def get_songs(_, message):
+    query = message.text
+    m = await message.reply_text("Searching", quote=True)
+    search = SearchVideos(f"{query}", offset=1, mode="dict", max_results=1)
+    mi = search.result()
+    mio = mi["search_result"]
+    lenk = mio[0]["link"]
+    title = mio[0]["title"]
+    ytid = mio[0]["id"]
+    channel = mio[0]["channel"]
+    #views = mio[0]["views"]
+    dur = mio[0]["duration"]
+    tblink = f"https://img.youtube.com/vi/{ytid}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    tb = wget.download(tblink)
     
-print("""SDBot is Started...""")  
+    opts = {
+        "format": "bestaudio",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "writethumbnail": True,
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "720",
+            }
+        ],
+        "outtmpl": "%(id)s.mp3",
+        "quiet": True,
+        "logtostderr": False,
+    }
+    
+    await m.edit("Downloading speed could be slow. Please hold on...")
+    try:
+        with YoutubeDL(opts) as ytdl:
+            ytdl_data = ytdl.extract_info(lenk, download=True)
+    except Exception as e:
+        return await m.edit(f"**Download Failed** \n\n```{e}```")
+      
+    cap = f"**🎧 Title:** {title} \n**🎥 Channel:** {channel} \n**⏳ Duration:** {dur} \n\n**📮 By @JaguarBots**"
+    aud = f"{ytdl_data['id']}.mp3"
+    await m.edit("Uploading")
+    await message.reply_audio(audio=open(aud, "rb"), 
+                              duration=int(ytdl_data["duration"]), 
+                              title=str(ytdl_data["title"]), 
+                              performer=str(ytdl_data["uploader"]),
+                              thumb=tb,
+                              caption=cap,
+                              quote=True)
+    await m.delete()
+    for files in (tb, aud):
+        if files and os.path.exists(files):
+            os.remove(files)
+
+#@SDBotz.on_message(filters.private & filters.text | filters.media)
+#async def AWBotz(client, message):
+#    await message.copy(message.chat.id)
+    
+print("""SDBot is Started...""")    
 SDBotz.run()
