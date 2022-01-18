40b86354dfd0edfcd1c7c826f2e7a82afdd8dd0f
szsupunma <86388269+szsupunma@users.noreply.github.com>
2022-01-17 22:54:59 -0800

    -1001511610738

--- Bot.py
+++ Bot.py
@@ -49,7 +49,7 @@ FSUBB = InlineKeyboardMarkup(
         InlineKeyboardButton(text="👨🏻‍💻 Bot Updates", url=f"https://t.me/SDBots_Inifinity") 
         ]]
     )
-LOG_CHANNEL = "-1001475839216"
+LOG_CHANNEL = "-1001511610738"
 @SDBotz.on_message(filters.command('start') & filters.private)
 async def start(client, message):
     try:
