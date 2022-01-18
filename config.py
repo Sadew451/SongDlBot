61d6517edfd5aee7979e407c590a8fb75b8b7b01
szsupunma <86388269+szsupunma@users.noreply.github.com>
2022-01-18 06:20:42 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-import os
 class Config(object):
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
 	DATABASE_URL = os.environ.get("DATABASE_URL")
-        DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
+DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
