7ab44ef06d782523eb8f41140efa684b74f3733e
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2022-01-18 13:59:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ class Config(object):
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	BOT_OWNER = os.environ.get("BOT_OWNER")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
